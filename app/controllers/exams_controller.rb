f6509b2c286399412987a8b14bb806d698c2c3ec
Nguyen Cuong <cuongnx2603@gmail.com>
2015-06-01 09:56:31 +0700

    user create exam with random questions

---
+++ app/controllers/exams_controller.rb
@@ -0,0 +1,39 @@
+class ExamsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :subjects_select_tag, only: [:new]
+
+  def index
+    @exams = current_user.exams.paginate page: params[:page]
+  end
+
+  def new
+    @exam = Exam.new
+  end
+
+  def create
+    @subject = Subject.find params[:subject_id]
+    @exam = current_user.exams.new exam_params
+    @exam.subject_id = @subject.id
+    if @exam.save
+      flash[:success] = t :success_flash
+      redirect_to exams_path
+    else
+      render 'new'
+    end
+  end
+
+  def show    
+    @exam = Exam.find params[:id]
+    @questions = @exam.questions
+  end
+ 
+  private
+
+  def exam_params
+    params.require(:exam).permit :name, :user_id, :subject_id
+  end    
+
+  def subjects_select_tag
+    @subjects = Subject.all.map{|c| [c.name, c.id]}
+  end    
+end
--- app/models/exam.rb
+++ app/models/exam.rb
@@ -3,4 +3,14 @@ class Exam < ActiveRecord::Base
   has_many :results, dependent: :destroy
   belongs_to :user
   belongs_to :subject
+
+  after_create :random_questions
+
+  private    
+
+  def random_questions
+    questions = self.subject.questions.order("RAND()").first Settings.default_num_questions_random
+    questions.each {|question| question.update_attributes exam_id: id}
+  end
+
 end
